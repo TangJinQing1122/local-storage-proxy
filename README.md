898414f7d83cfc8b0ce8b3f66f6536215eab2d40
TangJinQing <op444455@qq.com>
2025-02-14 19:37:31 +0800

    add.

---
+++ README.md
@@ -0,0 +1,11 @@
+# about
+Proxy you local-storage-data to obj.And autoupdate.
+
+# use
+```
+// const key = 'localKey';
+// const defaultConfig = { someSetting: true };
+
+// const localStorageProxy = new LocalStorageProxy(key, defaultConfig);
+// export const localKeyProxy = localStorageProxy.createProxy();
+```
---
+++ index.ts
@@ -0,0 +1,44 @@
+import { localStore } from 'dz-storage';
+import { Reactive, reactive, watch } from 'vue';
+
+interface ConfigInterface {
+  [key: string]: any;
+}
+
+class LocalStorageProxy<T extends ConfigInterface> {
+  private key: string;
+  private defaultConfig: T;
+
+  constructor(key: string, defaultConfig: T) {
+    this.key = key;
+    this.defaultConfig = defaultConfig;
+    
+    if (localStore.get(this.key) == null) {
+      localStore.set(this.key, this.defaultConfig);
+    }
+  }
+
+  public createProxy():Reactive<T> {
+    const storedConfig = localStore.get(this.key) as T;
+    const proxyConfig = reactive(storedConfig);
+
+    watch(
+      () => proxyConfig,
+      (newConfig) => {
+        localStore.set(this.key, newConfig);
+      },
+      {
+        deep: true,
+      }
+    );
+
+    return proxyConfig;
+  }
+}
+
+// // use Egg.
+// const key = 'localKey';
+// const defaultConfig = { someSetting: true };
+
+// const localStorageProxy = new LocalStorageProxy(key, defaultConfig);
+// export const localKeyProxy = localStorageProxy.createProxy();
